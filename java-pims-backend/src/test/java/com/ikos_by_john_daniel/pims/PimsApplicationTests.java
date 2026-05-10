0cf9e4c28d78fc5290fc0baf12294040f437298e
johdanike <chinedunomeh2015@gmail.com>
2026-05-10 18:54:21 +0100

    chore: Initialize dual-stack monorepo structure for PIMS

---
+++ java-pims-backend/src/test/java/com/ikos_by_john_daniel/pims/PimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikos_by_john_daniel.pims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
